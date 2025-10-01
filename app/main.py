618f6de9729bb35cca840b025ddfde16f5a4d590
surabhi-chandrakant <surabhibhor6@gmail.com>
2025-10-01 16:09:22 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -292,5 +292,5 @@ async def root():
     }
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
